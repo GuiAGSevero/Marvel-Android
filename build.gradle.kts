ff0249284021e58d606c7de270b1d0e48a0bfbec
Guilherme Severo <131295238+GuiAGSevero@users.noreply.github.com>
2025-06-14 09:23:38 -0300

    Feature/add testing module (#8)
    * Refactor: Move core model to domain model
    
    * feat: Add testing module and update dependencies
    
    ---------
    
    Co-authored-by: GuiSevero <guilherme_severo@sicredi.com.br>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,20 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.severo.marvel.build.Versions
 
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:8.10.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.0")
     }
 }
 
 plugins {
-    id("io.gitlab.arturbosch.detekt").version("1.23.8")
+    id("io.gitlab.arturbosch.detekt").version("1.18.0")
 }
 
 allprojects {
@@ -25,14 +26,4 @@ allprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-        txt.required.set(true)
-        sarif.required.set(true)
-        md.required.set(true)
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "Marvel"
 include(":app")
 include(":core")
+include(":testing")
